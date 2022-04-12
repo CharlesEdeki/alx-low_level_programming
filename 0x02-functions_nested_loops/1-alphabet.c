0c0b9d3d9dce44522c115632b421fa3c1e3df020
Charles Edeki <charlesedeki093@gmail.com>
2022-04-12 15:19:40 -0700

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,9 @@
 /**
  * main - check the code
  *
- * description: Prints the alphabets
+ * Description: Prints the alphabets
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
