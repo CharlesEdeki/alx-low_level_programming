8e9503a799a805a5ef66cf087067b55d018c5574
Charles Edeki <charlesedeki093@gmail.com>
2022-04-26 14:49:04 -0700

    hey

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
