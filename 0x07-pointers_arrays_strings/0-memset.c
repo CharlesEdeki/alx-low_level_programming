bb63ed03d235bdd64242182ee5237f141c59615a
Charles Edeki <charlesedeki093@gmail.com>
2022-04-26 14:24:48 -0700

    corrections

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 /**
  *_memset -   fills memory with a constant byte
- *@size: pointer block of memory to fill
- *@buffer: value to set
+ *@s: pointer block of memory to fill
+ *@b: value to set
  *@n: bytes of the memory
  *Return: dest
  */
